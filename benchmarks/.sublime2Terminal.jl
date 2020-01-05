e5ffee0c5b67e487d7dd964a4bcbcadc99086971
matthieugomez <matthieugomez@Matthieus-MacBook-Pro.local>
2020-01-05 08:27:45 -0800

    ok

--- benchmarks/.sublime2Terminal.jl
+++ benchmarks/.sublime2Terminal.jl
@@ -1,1 +1,11 @@
-@time solve_residuals!(deepcopy(x), fes; maxiter = 300)
+using FixedEffects, Random, Statistics
+Random.seed!(1234)
+N = 10000000
+K = 100
+id1 = rand(1:div(N, K), N)
+id2 = rand(1:K, N)
+fes = [FixedEffect(id1), FixedEffect(id2)]
+x = rand(N)
+
+# simple problem
+@time solve_residuals!(deepcopy(x), fes)
