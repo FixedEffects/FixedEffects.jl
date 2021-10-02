1d18b132174ede83d3301ac5248f24952b0eda7f
Matthieu Gomez <gomez.matthieu@gmail.com>
2021-10-02 11:46:24 -0400

    Update FixedEffectSolverCPU.jl
    Correct GLFixedEffectModels

--- src/FixedEffectSolvers/FixedEffectSolverCPU.jl
+++ src/FixedEffectSolvers/FixedEffectSolverCPU.jl
@@ -153,14 +153,9 @@ function solve_residuals!(r::AbstractVector, feM::FixedEffectSolverCPU{T}; tol::
 		feM.r .*=  sqrt.(feM.weights)
 	end
 	copyto!(feM.b, feM.r)
-	if length(feM.x.x) == 1
-		mul!(feM.x, feM.m', feM.b, 1, 0)
-		iter, converged = 1, true
-	else
-		mul!(feM.x, feM.m', feM.b, 1, 0)
-		x, ch = lsmr!(feM.x, feM.m, feM.b, feM.v, feM.h, feM.hbar; atol = tol, btol = tol, maxiter = maxiter)
-		iter, converged = ch.mvps + 1, ch.isconverged
-	end
+	mul!(feM.x, feM.m', feM.b, 1, 0)
+	x, ch = lsmr!(feM.x, feM.m, feM.b, feM.v, feM.h, feM.hbar; atol = tol, btol = tol, maxiter = maxiter)
+	iter, converged = ch.mvps + 1, ch.isconverged
 	mul!(feM.r, feM.m, feM.x, -1, 1)
 	if !(feM.weights isa UnitWeights)
 		feM.r ./=  sqrt.(feM.weights)
