b8d4a04a9103868b52ddb7d24c26891ce165aa41
Matthieu Gomez <gomez.matthieu@gmail.com>
2026-01-21 15:01:22 -0500

    requires 1.8

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,6 @@
 tests = ["types.jl", "solve.jl"]
 println("Running tests:")
 
-# A work around for tests to run on older versions of Julia
-using Pkg
-if VERSION >= v"1.8"
-	Pkg.add("Metal")
-	using Metal
-end
-
 using Test, StatsBase, CUDA, Metal, FixedEffects, PooledArrays, CategoricalArrays
 
 for test in tests
