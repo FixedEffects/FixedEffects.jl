f1a773869a20129baf8a49ee8a9e07d8704f7744
Matthieu Gomez <gomez.matthieu@gmail.com>
2026-01-28 20:15:20 -0500

    Reformat function signatures for consistency

--- src/AbstractFixedEffectSolver.jl
+++ src/AbstractFixedEffectSolver.jl
@@ -9,7 +9,7 @@ abstract type AbstractFixedEffectSolver{T} end
 works_with_view(::AbstractFixedEffectSolver) = false
 
 """
-`solve_residuals!(y, fes, w; method = :cpu, double_precision = method == :cpu, tol = 1e-8, maxiter = 10000, )`
+`solve_residuals!(y, fes, w; method = :cpu, double_precision = method == :cpu, tol = 1e-8, maxiter = 10000)`
 
 Returns ``y_i - X_i'\\beta`` where ``\\beta = argmin_{b} \\sum_i y_i - X_i'b``, where `X` denotes the matrix of fixed effects `fes`.
 
@@ -36,8 +36,10 @@ solve_residuals!(rand(10), [FixedEffect(p1), FixedEffect(p2)])
 ```
 """
 function solve_residuals!(y::Union{AbstractVector{<: Real}, AbstractMatrix{<: Real}}, fes::AbstractVector{<: FixedEffect}, w::AbstractWeights = uweights(eltype(y), size(y, 1)); 
-	method::Symbol = :cpu, double_precision::Bool = method == :cpu, 
-	tol::Real = double_precision ? 1e-8 : 1e-6, maxiter::Integer = 10000,
+	method::Symbol = :cpu, 
+	double_precision::Bool = method == :cpu, 
+	tol::Real = double_precision ? 1e-8 : 1e-6, 
+	maxiter::Integer = 10000,
 	nthreads = nothing)
 	any((length(fe) != size(y, 1) for fe in fes)) && throw("FixedEffects must have the same length as y")
 	any(ismissing.(fes)) && throw("FixedEffects must not have missing values")
@@ -152,9 +154,11 @@ solve_coefficients!(rand(10), [FixedEffect(p1), FixedEffect(p2)])
 ```
 """
 function solve_coefficients!(y::AbstractVector{<: Number}, fes::AbstractVector{<: FixedEffect}, w::AbstractWeights = uweights(eltype(y), length(y)); 
-		method::Symbol = :cpu, double_precision::Bool = method == :cpu, 
-	tol::Real = double_precision ? 1e-8 : 1e-6,  maxiter::Integer = 10000, 
-	nthreads = nothing)
+		method::Symbol = :cpu, 
+		double_precision::Bool = method == :cpu, 
+		tol::Real = double_precision ? 1e-8 : 1e-6,  
+		maxiter::Integer = 10000, 
+		nthreads = nothing)
 	any(ismissing.(fes)) && throw("Some FixedEffect has a missing value for reference or interaction")
 	any((length(fe) != length(y) for fe in fes))  && throw("FixedEffects must have the same length as y")
 	feM = AbstractFixedEffectSolver{double_precision ? Float64 : Float32}(fes, w, Val{method}, nthreads)
